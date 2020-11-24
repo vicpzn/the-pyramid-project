267018ab2956703fd83d871d7d5d59e31eea5901
Victorien Provenzano <victorien.provenzano@icloud.com>
2020-11-24 14:45:49 +0100

    factorized

--- js/index.js
+++ js/index.js
@@ -1,27 +1,24 @@
+// WELCOME WINDOW & TOP BAR
+
 const startBtn = document.querySelector("#start-btn");
 const welcomeWindow = document.querySelector("#welcome");
 const goBackBtn = document.querySelector("#go-back-btn");
 const instructionsBtn = document.querySelector("#instructions-btn");
 const instructionsWindow = document.querySelector("#instructions");
 const instructionTopBar = document.querySelector("#instructions-link");
-const firstPromptWindows = document.querySelector("#first-prompt");
 
-startBtn.onclick = hideWelcome;
-instructionsBtn.onclick = showInstructions;
-goBackBtn.onclick = hideInstructions;
-instructionTopBar.onclick = showInstructions;
-
-function hideWelcome() {
-  welcomeWindow.style.visibility = "hidden";
+function hide(arr) {
+  arr.style.visibility = "hidden";
 }
 
-function hideInstructions() {
-  instructionsWindow.style.visibility = "hidden";
+function show(arr) {
+  arr.style.visibility = "visible";
 }
 
-function showInstructions() {
-  instructionsWindow.style.visibility = "visible";
-}
+startBtn.addEventListener("click", () => hide(welcomeWindow));
+goBackBtn.addEventListener("click", () => hide(instructionsWindow));
+instructionsBtn.addEventListener("click", () => show(instructionsWindow));
+instructionTopBar.addEventListener("click", () => show(instructionsWindow));
 
 // DECK CLICKABLE
 
@@ -29,5 +26,56 @@ const deckCards = document.querySelector("#deck");
 deckCards.onclick = draw;
 
 function draw() {
-  firstPromptWindows.style.visibility = "visible";
+  firstPromptWindow.style.visibility = "visible";
 }
+
+// FIRST PROMPT
+
+const firstPromptWindow = document.querySelector("#first-prompt");
+const redBtn = document.querySelector("#red-card-btn");
+const blackBtn = document.querySelector("#black-card-btn");
+
+function updateScore() {
+  player1.score += 10;
+}
+
+function redAssign() {
+  player1.firstChoice = "Red";
+}
+
+function blackAssign() {
+  player1.firstChoice = "Black";
+}
+
+redBtn.addEventListener("click", () => console.log("red clicked"));
+blackBtn.addEventListener("click", () => console.log("black clicked"));
+
+// START -> ASSIGN CARDS TO THE 4 PLAYERS
+
+// FIRST ROUND:
+// player1.firstChoice = "Red" or "Black"
+// flip the card, update score
+// randomize left, flip the card, update score
+// randomize top, flip the card, update score
+// randomize bottom, flip the card, update score
+
+// SECOND ROUND:
+// player1.secondChoice = "Higher" or "Lower"
+// flip the card, update score
+// randomize left, flip the card, update score
+// randomize top, flip the card, update score
+// randomize bottom, flip the card, update score
+
+// THIRD ROUND:
+// player1.thirdChoice = "Inside" or "Outside"
+// flip the card, update score
+// randomize left, flip the card, update score
+// randomize top, flip the card, update score
+// randomize bottom, flip the card, update score
+
+// FOURTH ROUND:
+// player1.fourthChoice = "H" "S" "C" or "D"
+// flip the card, update score
+// randomize left, flip the card, update score
+// randomize top, flip the card, update score
+// randomize bottom, flip the card, update score
--- js/players.js
+++ js/players.js
@@ -1,27 +1,3 @@
-// PLAYERS
-
-class Player {
-  constructor(name) {
-    this.points = 0;
-    this.name = name;
-    this.firstChoice = "";
-    this.secondChoice = "";
-    this.thirdChoice = "";
-    this.fourthChoice = "";
-    this.firstCard = "";
-    this.secondCard = "";
-    this.thirdCard = "";
-    this.fourthCard = "";
-  }
-}
-
-const player1 = new Player("You");
-const player2 = new Player("Left");
-const player3 = new Player("Right");
-const player4 = new Player("Bottom");
-
-// DECK
-
 var family = ["Spades", "Diamonds", "Clubs", "Hearts"];
 var values = [
   "2",
@@ -41,22 +17,25 @@ var values = [
 
 var deck = [];
 
-function initDeck() {
+function getDeck() {
   for (var i = 0; i < family.length; i++) {
     for (var j = 0; j < values.length; j++) {
       var cards = `${values[j]} ${family[i]}`;
-      //   var card = { Value: values[j], Family: family[i] };
       deck.push(cards);
     }
   }
   return deck;
 }
 
-initDeck();
+getDeck();
+
+// a bit of randomization
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+// get a card and remove it from the deck
 
 function getCard() {
   var card = deck[getRandomInt(deck.length)];
@@ -65,108 +44,135 @@ function getCard() {
   return card;
 }
 
-player1.firstCard = getCard();
-player1.secondCard = getCard();
-player1.thirdCard = getCard();
-player1.fourthCard = getCard();
-
-// COMPUTER CHOICES
+// different choices
 
 var firstRoundChoices = ["Red", "Black"];
 var secondRoundChoices = ["Lower", "Higher"];
 var thirdRoundChoices = ["Inside", "Outside"];
 var fourthRoundChoices = ["Spades", "Diamonds", "Clubs", "Hearts"];
 
-player1.firstChoice = firstRoundChoices[getRandomInt(2)];
-player1.secondChoice = secondRoundChoices[getRandomInt(2)];
-player1.thirdChoice = thirdRoundChoices[getRandomInt(2)];
-player1.fourthChoice = fourthRoundChoices[getRandomInt(4)];
+// Players
 
-const playerOneFirstCardValue = Number(
-  player1.firstCard.substr(0, player1.firstCard.indexOf(" "))
-);
-const playerOneSecondCardValue = Number(
-  player1.secondCard.substr(0, player1.secondCard.indexOf(" "))
-);
-const playerOneThirdCardValue = Number(
-  player1.thirdCard.substr(0, player1.thirdCard.indexOf(" "))
-);
+class Player {
+  constructor(name) {
+    this.points = 0;
+    this.name = name;
+    this.firstChoice = "Red";
+    this.secondChoice = "Lower";
+    this.thirdChoice = "Inside";
+    this.fourthChoice = "Spades";
+    this.firstCard = getCard();
+    this.secondCard = getCard();
+    this.thirdCard = getCard();
+    this.fourthCard = getCard();
+  }
 
-// first test
+  randomChoices() {
+    this.firstChoice = firstRoundChoices[getRandomInt(2)];
+    this.secondChoice = secondRoundChoices[getRandomInt(2)];
+    this.thirdChoice = thirdRoundChoices[getRandomInt(2)];
+    this.fourthChoice = fourthRoundChoices[getRandomInt(4)];
+  }
 
-if (
-  (player1.firstChoice === "Red" && player1.firstCard.includes("Hearts")) ||
-  (player1.firstChoice === "Red" && player1.firstCard.includes("Diamonds"))
-) {
-  player1.points += 10;
-} else if (
-  (player1.firstChoice === "Black" && player1.firstCard.includes("Clubs")) ||
-  (player1.firstChoice === "Black" && player1.firstCard.includes("Spades"))
-) {
-  player1.points += 10;
+  firstRound() {
+    if (
+      (this.firstChoice === "Red" && this.firstCard.includes("Hearts")) ||
+      (this.firstChoice === "Red" && this.firstCard.includes("Diamonds"))
+    ) {
+      this.points += 10;
+    } else if (
+      (this.firstChoice === "Black" && this.firstCard.includes("Clubs")) ||
+      (this.firstChoice === "Black" && this.firstCard.includes("Spades"))
+    ) {
+      this.points += 10;
+    }
+  }
+
+  secondRound() {
+    const playerFirstCardValue = Number(
+      this.firstCard.substr(0, this.firstCard.indexOf(" "))
+    );
+    const playerSecondCardValue = Number(
+      this.secondCard.substr(0, this.secondCard.indexOf(" "))
+    );
+    if (
+      this.secondChoice === "Lower" &&
+      playerSecondCardValue < playerFirstCardValue
+    ) {
+      this.points += 20;
+      console.log("+20");
+    } else if (
+      this.secondChoice === "Higher" &&
+      playerSecondCardValue > playerFirstCardValue
+    ) {
+      this.points += 20;
+      console.log("+20");
+    }
+  }
+
+  thirdRound() {
+    var comparison = [];
+    const playerFirstCardValue = Number(
+      this.firstCard.substr(0, this.firstCard.indexOf(" "))
+    );
+    const playerSecondCardValue = Number(
+      this.secondCard.substr(0, this.secondCard.indexOf(" "))
+    );
+    const playerThirdCardValue = Number(
+      this.thirdCard.substr(0, this.thirdCard.indexOf(" "))
+    );
+    comparison.push(playerFirstCardValue, playerSecondCardValue);
+    comparison.sort((a, b) => a - b);
+    console.log(comparison);
+
+    function inside() {
+      return (
+        playerThirdCardValue > comparison[0] &&
+        playerThirdCardValue < comparison[1]
+      );
+    }
+
+    if (player1.thirdChoice === "Inside" && inside()) {
+      player1.points += 30;
+    } else if (player1.thirdChoice === "Outside" && !inside()) {
+      player1.points += 30;
+    }
+  }
+
+  fourthRound() {
+    if (this.fourthCard.includes(this.fourthChoice)) {
+      this.points += 40;
+    }
+  }
 }
 
-// second test
+const player1 = new Player("You");
+const player2 = new Player("Left");
+const player3 = new Player("Right");
+const player4 = new Player("Bottom");
 
-if (
-  player1.secondChoice === "Lower" &&
-  playerOneSecondCardValue < playerOneFirstCardValue
-) {
-  player1.points += 20;
-} else if (
-  player1.secondChoice === "Higher" &&
-  playerOneSecondCardValue > playerOneFirstCardValue
-) {
-  player1.points += 20;
-}
+player2.randomChoices();
+player3.randomChoices();
+player4.randomChoices();
 
-// third test
+player1.firstRound();
+player2.firstRound();
+player3.firstRound();
+player4.firstRound();
 
-// extract values till the first space and sort them
+player1.secondRound();
+player2.secondRound();
+player3.secondRound();
+player4.secondRound();
 
-var comparison = [];
-comparison.push(playerOneFirstCardValue, playerOneSecondCardValue);
-comparison.sort((a, b) => a - b);
+player1.thirdRound();
+player2.thirdRound();
+player3.thirdRound();
+player4.thirdRound();
 
-if (
-  player1.thirdChoice === "Inside" &&
-  comparison[0] < playerOneThirdCardValue &&
-  playerOneThirdCardValue < comparison[1]
-) {
-  player1.points += 30;
-} else if (
-  player1.thirdChoice === "Outside" &&
-  comparison[0] > playerOneThirdCardValue &&
-  playerOneThirdCardValue > comparison[1]
-) {
-  player1.points += 30;
-}
+player1.fourthRound();
+player2.fourthRound();
+player3.fourthRound();
+player4.fourthRound();
 
-// fourth test
-
-if (player1.fourthCard.includes(player1.fourthChoice)) {
-  player1.points += 40;
-}
-
-console.log(player1);
-
-// function renderDeck(deck) {
-//   document.getElementById("deck").innerHTML = "";
-
-//   for (let i = 0; i < deck.length; i++) {
-//     let card = document.createElement("div");
-//     let value = document.createElement("div");
-//     let suit = document.createElement("div");
-//     card.className = "card";
-//     value.className = "value";
-//     suit.className = "suit " + deck[i].Suit;
-
-//     value.innerHTML = deck[i].Value;
-//     card.appendChild(value);
-//     card.appendChild(suit);
-
-//     document.getElementById("deck").appendChild(card);
-//   }
-// }
-
-// renderDeck(deck1);
+console.log(player1, player2, player3, player4);
